8c39fba72b77b4556d860714f9521fbd5b822f85
EduardoMinjares <104042688+EduardoMinjares@users.noreply.github.com>
2023-05-24 20:38:00 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -6,5 +6,20 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        """Stuff to do before every test."""
 
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_valid_word(self):
+
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["D", "O", "G", "G", "G"], 
+                                 ["D", "O", "G", "G", "G"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"]]
+            response = self.client.get('/check-word?word=cat')
+            self.assertEqual(response.json['result'], 'ok')
